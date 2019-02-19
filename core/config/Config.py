d8cd32ecba0eb04b817fcb1159e8d840b4f36291
dustin-simon <dustin.simon@8mylez.com>
2019-02-19 10:35:17 +0100

    create config class

--- core/config/Config.py
+++ core/config/Config.py
@@ -1,25 +1,44 @@
-from abc import abstractmethod
-from abc import ABC
+import os.path
+from core.config.ConfigReader import ConfigReader
 
-class Config(ABC):
+class Config():
 
-    def __init__(self, properties):
-        self.properties = properties
+    def __init__(self, path, reader=None):
 
-    @abstractmethod
+        if path == None:
+            raise ValueError("No file path given.")
+
+        if reader == None:
+            self.reader = ConfigReader()
+        else:
+            self.reader = reader
+
+        self.path = path
+        self.loaded = False
+
     def load(self):
-        pass
+
+        self.properties = self.reader.load(self.path)
+        self.loaded = True
 
     def print(self):
         for key, value in self.properties.items():
             print(key + ": " + value)
 
     def get(self, propertyname):
+
+        if self.loaded != True:
+            raise RuntimeError("You must load the configuration first.")
+
         if propertyname in self.properties:
             return self.properties[propertyname]
         else:
             raise ValueError("Property '" + propertyname + "' not available.")
+            
 
     def getProperties(self):
+        if self.loaded != True:
+            raise RuntimeError("You must load the configuration first.")
+
         return self.properties
         
