13e8cb22b9b9a73d3a1e14edccd41ec0426c8173
Unknown <dustin.simon@8mylez.com>
2019-05-27 22:14:29 +0200

    creation of CreateAreaHandler

---
+++ ext/default/actions/createArea/CreateAreaHandler.py
@@ -0,0 +1,12 @@
+from modules.action.ActionHandler import ActionHandler
+from core.application.Application import Application
+from modules.area.Area import Area
+import json
+
+class CreateAreaHandler(ActionHandler):
+    
+    def handleAction(self, jsonData):
+
+        
+        
+        return json.dumps(jsonResponse)
