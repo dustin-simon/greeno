6b0e6eef69391c541f4ff5c9e8d26a9e66a4f5d3
Dustin <dustin.simon@8mylez.com>
2020-01-18 19:41:02 +0100

    add web interface to equipment

--- start.py
+++ start.py
@@ -5,7 +5,5 @@ from core.application.GreenoApplication import GreenoApplication
 configFile = "../system.xml"
 app = GreenoApplication(configFile)
 
-print(app.config.getProperties())
-
 app.start()
 app.getStatusObserver().join()
